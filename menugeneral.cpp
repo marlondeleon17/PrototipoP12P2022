778ef73003bccb0ca36743d96278126e6cfcf81d
marlondeleon17 <mdeleong17@mi.umg.gt>
2022-04-21 14:40:31 -0600

    menu general

---
+++ menugeneral.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+
+using namespace std;
+int selection;
+void menu_general (void )
+{
+
+   do{
+
+
+
+
+
+    cout << "------------Menu general: Marlon Ruben De Leon Gonzalez 9959-19-460--------" <<endl;
+
+	cout << "indica que opcion va a seleccionar: " << endl;
+
+	cout << "(1)Catalogos---" << endl;
+
+	cout << "(2)Procesos-------" << endl;
+
+	cout << "(3)Informes------" << endl;
+
+	cout << "(4)salir-------------------------------" << endl;
+
+	cout << "su opcion a elegir es: " << endl;
+
+	cin >> selection;
+
+	cout << endl << endl;
+
+	}while(selection !=4);
+}
